055fe7b523596c7e512efe8df625166e1b5c140b
Piotr Mamenas <piotr.mamenas@gmail.com>
2017-09-30 22:41:55 +0200

    Adding bootbox to contact deletion in datatable, the user is prompted if he is sure he wants to delete now.

--- Web/Content/app/controllers/contact/contactTableController.js
+++ Web/Content/app/controllers/contact/contactTableController.js
@@ -4,42 +4,42 @@
 
     var initializeDatatable = function (result) {
         table = $("#contactTable").DataTable({
-        data: result,
-        columns: [
-            {
-                data: "Name"
-            },
-            {
-                data: "FirstName"
-            },
-            {
-                data: "LastName"
-            },
-            {
-                data: "Email"
-            },
-            {
-                data: "PhoneNumber"
-            },
-            {
-                data: "Partner.Name"
-            },
-            {
-                data: "Id",
-                render: function (data) {
-                    return "<a href=\"" + "update/" + data + "\" class=\"btn btn-default btn-block\"><span class='fa fa-pencil'></span>";
+            data: result,
+            columns: [
+                {
+                    data: "Name"
+                },
+                {
+                    data: "FirstName"
+                },
+                {
+                    data: "LastName"
+                },
+                {
+                    data: "Email"
+                },
+                {
+                    data: "PhoneNumber"
+                },
+                {
+                    data: "Partner.Name"
+                },
+                {
+                    data: "Id",
+                    render: function (data) {
+                        return "<a href=\"" + "update/" + data + "\" class=\"btn btn-default btn-block\"><span class='fa fa-pencil'></span>";
+                    }
+                },
+                {
+                    data: "Id",
+                    render: function (data) {
+                        return "<button href=\"#\" data-contact-id=\"" + data + "\" class=\"btn btn-default btn-block contact-delete-contact\"><span class='fa fa-trash'></span></button>";
+                    }
                 }
-            },
-            {
-                data: "Id",
-                render: function(data) {
-                    return "<button href=\"#\" data-contact-id=\"" + data + "\" class=\"btn btn-default btn-block contact-delete-contact\"><span class='fa fa-trash'></span></button>";
-                }
+            ],
+            language: {
+                emptyTable: "No records at present."
             }
-        ],
-        language: {
-            emptyTable: "No records at present."
-        }
         });
     }
 
@@ -47,27 +47,48 @@
         button = $(e.target);
 
         service.deleteContact(button.attr("data-contact-id"),
-            function() {
+            function () {
                 table.row(button.parents("tr"))
                     .remove()
                     .draw();
             },
-            function() {
-                alert("Something went wrong");
+            function () {
+                alert("Unexpected Error");
             });
+    };
+
+    var openDeletePrompt = function (e) {
+
+        bootbox.confirm({
+            title: "Delete contact?",
+            message: "Are you sure you want to delete this contact? This cannot be undone.",
+            buttons: {
+                cancel: {
+                    label: "<i class=\"fa fa-times\"></i> Cancel"
+                },
+                confirm: {
+                    label: "<i class=\"fa fa-check\"></i> Confirm"
+                }
+            },
+            callback: function (isConfirmed) {
+                if (isConfirmed === true) {
+                    deleteSelectedRow(e);
+                }
+            }
+        });
     };
 
     var init = function () {
 
         var loadDatatable = function (result) {
             initializeDatatable(result);
-            $(".contact-delete-contact").on("click", deleteSelectedRow);
+            $(".contact-delete-contact").on("click", openDeletePrompt);
         }
         service.getContacts(loadDatatable, loadDatatable);
-    }
+    };
 
     return {
         init: init
     };
 
-}(contactService)
+}(contactService);
