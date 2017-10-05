4c81f1b0e6cc26459e3a9fcea19e4a98cc8590a7
Piotr Mamenas <piotr.mamenas@gmail.com>
2017-10-06 01:20:39 +0200

    Adding form to open a new account, adjusting sitemap to make the new account button route to the form, adding account view model, adding relevant controller actions for the above. Scaffolding initial view for Updating account.

---
+++ Web.Presentation/ViewModels/ContactViewModels/AccountViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.Mvc;
+using Service.Dtos;
+
+namespace Web.Presentation.ViewModels.ContactViewModels
+{
+    public class AccountViewModel
+    {
+        public int? Id { get; set; }
+
+        [DisplayName("Account Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Account Number")]
+        public string Number { get; set; }
+
+        [DisplayName("Related Partner")]
+        public int SelectedPartnerId { get; set; }
+
+        public IEnumerable<SelectListItem> PartnerNumberSelection { get; set; }
+    }
+}
--- Web/Controllers/AccountController.cs
+++ Web/Controllers/AccountController.cs
@@ -1,15 +1,32 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Core.Domain.Accounts;
+using Core.Domain.Partners;
+using Core.Interfaces;
+using Core.Interfaces.Repositories;
+using Infrastructure.AutoMapper;
 using Ninject.Extensions.Logging;
 using Web.Controllers.Templates;
+using Web.Presentation.ViewModels.ContactViewModels;
 
 namespace Web.Controllers
 {
     [RoutePrefix("accounts")]
     public class AccountController : BaseController
     {
-        public AccountController(ILogger logger)
+        private readonly IComplete _unitOfWork;
+        private readonly IAccountRepository<Account> _accounts;
+        private readonly IPartnerRepository<Partner> _partners;
+
+        public AccountController(IUnitOfWork unitOfWork, ILogger logger)
             : base(logger)
         {
+            _unitOfWork = (IComplete) unitOfWork;
+            _accounts = unitOfWork.Accounts;
+            _partners = unitOfWork.Partners;
         }
 
         [Route("")]
@@ -22,6 +39,59 @@ namespace Web.Controllers
         public ActionResult List()
         {
             return View();
+        }
+
+        [Route("create")]
+        public ActionResult OpenAccount()
+        {
+            var partnerNumberSelection = new AccountViewModel
+            {
+                PartnerNumberSelection = GetPartnerSelection()
+            };
+
+            return View(partnerNumberSelection);
+        }
+
+        [HttpPost, Route("create")]
+        public async Task<ActionResult> OpenAccount(AccountViewModel accountVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(accountVm);
+            }
+
+            var account = new Account();
+            var partner = await _partners.GetAsync(accountVm.SelectedPartnerId);
+
+            var validationResult = account.OpenNewAccount(accountVm.Name, accountVm.Number, partner);
+
+            // TODO: Add validation message display for complex validation
+            if (validationResult.Any())
+            {
+                return View();
+            }
+
+            _accounts.Add(account);
+
+            await _unitOfWork.CompleteAsync();
+
+            return RedirectToAction("List");
+        }
+
+        /// <summary>
+        /// Returns a list of partners available to be linked to the contact
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<SelectListItem> GetPartnerSelection()
+        {
+            var partnerSelection = _partners.GetAll()
+                .Select(p => new SelectListItem
+                {
+                    Value = p.Id.ToString(),
+                    Text = p.Number
+                });
+
+            return new SelectList(partnerSelection, "Value", "Text");
         }
     }
 }
