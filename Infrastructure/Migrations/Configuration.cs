15750a2726e80b57388f5e3a34a33eae63b94757
Piotr Mamenas <piotr.mamenas@gmail.com>
2017-11-08 13:54:56 +0100

    Also moving over Identity seeding into a separate class, this can be static as it will never have to be mocked plus it already has additional dependencies on Identity Framework user and role managers which Microsoft unfortunately didn't feel like extending over an interface.

--- Infrastructure/Migrations/Configuration.cs
+++ Infrastructure/Migrations/Configuration.cs
@@ -1,20 +1,6 @@
-using System;
-using System.Data.Entity;
 using System.Data.Entity.Migrations;
-using System.Linq;
 using System.Threading.Tasks;
-using Core.Domain.Accounts;
-using Core.Domain.Assets;
-using Core.Domain.Contacts;
-using Core.Domain.Countries;
-using Core.Domain.Currencies;
-using Core.Domain.Identity;
-using Core.Domain.Institutions;
-using Core.Domain.Partners;
-using Core.Domain.Portfolios;
-using Infrastructure.Identity;
-using Infrastructure.SeedData;
-using Infrastructure.SeedData.Test;
+using Infrastructure.SeedData.Identity;
 
 namespace Infrastructure.Migrations
 {
@@ -38,53 +24,8 @@ namespace Infrastructure.Migrations
             //AssetSeeder.Seed(context.Assets);
             //SeedPortfolio(context.Portfolios);
 
-            Task.Run(async () => { await SeedIdentityAsync(context); }).Wait();
+            Task.Run(async () => { await StaticIdentitySeeder.SeedIdentityAsync(context); }).Wait();
             context.SaveChanges();
-        }
-
-        private static async Task SeedIdentityAsync(ApplicationDbContext context)
-        {
-            var userManager = new ApplicationUserManager(new ApplicationUserStore(context));
-            var roleManager = new ApplicationRoleManager(new ApplicationRoleStore(context));
-
-            if (!roleManager.Roles.Any())
-            {
-                await roleManager.CreateAsync(new Role { Name = Role.AdminRole, Id = Guid.NewGuid().ToString()});
-                await roleManager.CreateAsync(new Role { Name = Role.AssociateRole, Id = Guid.NewGuid().ToString() });
-            }
-
-            if (!userManager.Users.Any(u => u.UserName == "DemoAdmin"))
-            {
-                var user = new User
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    UserName = "DemoAdmin",
-                    Email = "demoadmin@gmail.com",
-                    EmailConfirmed = true,
-                    PhoneNumber = "+41790799495",
-                    PhoneNumberConfirmed = true
-                };
-
-                await userManager.CreateAsync(user, "Secret1#");
-                await userManager.AddToRoleAsync(user.Id, Role.AdminRole);
-                await userManager.AddToRoleAsync(user.Id, Role.AssociateRole);
-            }
-
-            if (!userManager.Users.Any(u => u.UserName == "DemoUser"))
-            {
-                var user = new User
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    UserName = "DemoUser",
-                    Email = "demouser@gmail.com",
-                    EmailConfirmed = true,
-                    PhoneNumber = "+41780349204",
-                    PhoneNumberConfirmed = true
-                };
-
-                await userManager.CreateAsync(user, "Secret1#");
-                await userManager.AddToRoleAsync(user.Id, Role.AssociateRole);
-            }
         }
     }
 }
