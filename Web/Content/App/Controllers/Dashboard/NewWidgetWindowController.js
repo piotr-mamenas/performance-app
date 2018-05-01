25dba8adf156ee853dd53b3250a3dd85edf71b87
Piotr Mamenas <piotr.mamenas@gmail.com>
2018-05-01 13:02:58 +0200

    Adding widget icon spinner feature into create new widget form window.

--- Web/Content/App/Controllers/Dashboard/NewWidgetWindowController.js
+++ Web/Content/App/Controllers/Dashboard/NewWidgetWindowController.js
@@ -1,14 +1,33 @@
 ﻿var NewWidgetWindowController = function(resourceService) {
     var availableIcons = [];
+    var currentlySelectedIconId = 0;
 
-    var addIconImageDisplay = function () {
-        availableIcons.forEach(function(icon) {
-            $(".new-widget-icon-spinner").append("<span class='" + icon + "' display='none'></span>");
-        });
-        $(".new-widget-icon-spinner").first().show();
+    var appendIcon = function () {
+        console.log(availableIcons[currentlySelectedIconId]);
+        console.log(currentlySelectedIconId);
+        $(".new-widget-icon-spinner").empty();
+        $(".new-widget-icon-spinner").append("<span class='" + availableIcons[currentlySelectedIconId] + "'></span>");
     }
 
-    var onNewWidgetClick = function() {
+    var onLeftCaretClick = function (e) {
+        e.preventDefault();
+        currentlySelectedIconId = currentlySelectedIconId - 1;
+        if (currentlySelectedIconId < 0) {
+            currentlySelectedIconId = availableIcons.length - 1;
+        }
+        appendIcon();
+    };
+
+    var onRightCaretClick = function (e) {
+        e.preventDefault();
+        currentlySelectedIconId = currentlySelectedIconId + 1;
+        if (currentlySelectedIconId >= availableIcons.length - 1) {
+            currentlySelectedIconId = 0;
+        }
+        appendIcon();
+    };
+
+    var onNewWidgetClick = function () {
         bootbox.dialog({
             title: "Create New Widget",
             onEscape: function() {
@@ -29,11 +48,15 @@
                 }
             }
         });
+        $(".new-widget-caret-left").on("click", onLeftCaretClick);
+        $(".new-widget-caret-right").on("click", onRightCaretClick);
     };
 
     var init = function() {
         $(".open-new-widget-window").parent("a").on("click", onNewWidgetClick);
-        availableIcons = resourceService.getAvailableIcons(addIconImageDisplay, console.log);
+        resourceService.getAvailableIcons(function(data) {
+            availableIcons = data;
+        }, console.log);
     };
 
     return {
